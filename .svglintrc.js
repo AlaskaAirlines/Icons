4be0d4b18c60ec76173ffd3384c46d3bd2ea4dfd
Dale Sande <dale.sande@gmail.com>
2018-12-28 17:11:33 -0800

    add icons and update tests

--- .svglintrc.js
+++ .svglintrc.js
@@ -2,8 +2,7 @@ module.exports = {
     rules: {
         elm: {
             "svg": 1,
-            "svg > title": 1,
-            "g": true,
+            "svg > title": 1
         },
         attr: [
             { // ensure that the SVG element has the appropriate attributes
@@ -15,6 +14,16 @@ module.exports = {
                 "version": "1.1",
                 "xmlns:xlink": "http://www.w3.org/1999/xlink",
                 "xmlns": "http://www.w3.org/2000/svg",
+                "fill": false,
+                "style": false,
+                "xmlns:dc": false,
+                "xmlns:cc": false,
+                "xmlns:rdf": false,
+                "xmlns:svg": false,
+                "xmlns:sodipodi": false,
+                "xmlns:inkscape": false,
+                "x": false,
+                "y": false,
 
                 "rule::selector": "svg",
                 "rule::whitelist": true
@@ -24,6 +33,20 @@ module.exports = {
 
                 "rule::selector": "svg > title",
                 "rule::whitelist": true
+            },
+            { // ensure that a g element has the appropriate attributes
+                "fill": false,
+                "style": false,
+                "id": false,
+
+                "rule::selector": "svg g"
+            },
+            { // ensure that a path element has the appropriate attributes
+                "fill": false,
+                "style": false,
+                "id": false,
+
+                "rule::selector": "svg path"
             }
         ]
     }
