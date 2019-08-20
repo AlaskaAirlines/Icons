020d4957881e3d24ead11a88b637086bf92b2b72
Dale Sande <dale.sande@alaskaair.com>
2019-08-19 17:54:20 -0700

    feat: update build process to support PNG exports

--- scripts/build.js
+++ scripts/build.js
@@ -14,7 +14,8 @@ const buildIconsDir = `${__dirname}/../dist/icons`;
 const data = require(dataFile);
 const fileSys = require('file-system');
 const fs = require('fs');
-const StyleDictionary = require('style-dictionary').extend('./scripts/tokensConfig.json');
+const LocalTokens = require('style-dictionary').extend('./scripts/tokensConfig.json');
+const DesignTokens = require('style-dictionary').extend('./scripts/colorTokensConfig.json');
 
 const { titleToFilename } = require('./utils');
 
@@ -24,6 +25,8 @@ if (!fileSys.existsSync(icoDir)){
   fileSys.mkdirSync(icoDir);
 }
 
+// export JS versions of Icons
+// =======================================================================
 const icons = {};
 data.icons.forEach(icon => {
   const filename = titleToFilename(icon.title);
@@ -49,14 +52,75 @@ data.icons.forEach(icon => {
   // write new SVGs to ./dist
   fs.writeFileSync( `${buildIconsDir}/${filename}.svg`, icon.svg);
 
-  console.log(`${filename}.js / ${filename}.svg written to ./dist dir`)
+  // console.log(`${filename}.js / ${filename}.svg written to ./dist dir`)
+});
+
+// export 16px PNGs versions of Icons; default colors
+// =======================================================================
+const pngIcons = {};
+const SizeMedIcons = data.icons.filter(function(sizeData) {
+  return sizeData.PngSize == 20;
+});
+
+SizeMedIcons.forEach(icon => {
+  const filename = titleToFilename(icon.title);
+  icon.svg = fs.readFileSync(`${iconsDir}/${filename}.svg`, 'utf8');
+  const insertPos = icon.svg.indexOf("svg") + 4;
+  const elementStyle = `style="fill: ${icon.PngColor}" `;
+  const iconStyle = `
+  <style xmlns="http://www.w3.org/2000/svg" type="text/css"><![CDATA[
+    @import url("../../dist/tokens/colorToken.css");
+  ]]></style>
+  `
+
+  icon.svg = [icon.svg.slice(0, insertPos), elementStyle, icon.svg.slice(insertPos)].join('');
+  icon.svg = icon.svg.replace("<style></style>", `${iconStyle}`);
+
+  icons[icon.title] = icon;
+
+  // write new SVGs to ./dist
+  fs.writeFileSync( `${buildIconsDir}/${filename}--20@2.png`, icon.svg);
+  fs.writeFileSync( `${buildIconsDir}/${filename}--20@3.png`, icon.svg);
+
+  // console.log(`${filename}.js / ${filename}.png written to ./dist dir`)
+});
+
+// export 16px PNGs versions of Icons; alt colors
+// =======================================================================
+const pngIconsAlt = {};
+const altColorSet = data.icons.filter(function(altData) {
+  return altData.AltPngColor && altData.PngSize == 20;
+});
+
+altColorSet.forEach(icon => {
+  const filename = titleToFilename(icon.title);
+  icon.svg = fs.readFileSync(`${iconsDir}/${filename}.svg`, 'utf8');
+  const insertPos = icon.svg.indexOf("svg") + 4;
+  const elementStyle = `style="fill: ${icon.AltPngColor}" `;
+  const iconStyle = `
+  <style xmlns="http://www.w3.org/2000/svg" type="text/css"><![CDATA[
+    @import url("../../dist/tokens/colorToken.css");
+  ]]></style>
+  `
+
+  icon.svg = [icon.svg.slice(0, insertPos), elementStyle, icon.svg.slice(insertPos)].join('');
+  icon.svg = icon.svg.replace("<style></style>", `${iconStyle}`);
+
+  icons[icon.title] = icon;
+
+  // write new SVGs to ./dist
+  fs.writeFileSync( `${buildIconsDir}/${filename}-alt--20@2.png`, icon.svg);
+  fs.writeFileSync( `${buildIconsDir}/${filename}-alt--20@3.png`, icon.svg);
+
+  // console.log(`${filename}.js / ${filename}.png written to ./dist dir`)
 });
 
 
 // Style Dictionary build function
-StyleDictionary.buildPlatform('SassVariables');
-StyleDictionary.buildPlatform('CSSCustomElements');
-StyleDictionary.buildPlatform('CSSCustomElements_SassFiletype');
+LocalTokens.buildPlatform('SassVariables');
+LocalTokens.buildPlatform('CSSCustomElements');
+LocalTokens.buildPlatform('CSSCustomElements_SassFiletype');
+DesignTokens.buildPlatform('colorTokens');
 
 console.log('')
 console.log('         .         . ')
