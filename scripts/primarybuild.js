915ca404063f652fefb79d064c19b3461b9c7373
Dominic Valenciana <dominic.valenciana@alaskaair.com>
2020-02-11 10:04:06 -0800

    refactor: removed extra comments

--- scripts/primarybuild.js
+++ scripts/primarybuild.js
@@ -189,13 +189,10 @@ function fileHeader(options) {
 }
 
 function titleCase(str) {
-  var splitStr = str.toLowerCase().split(' ');
-  for (var i = 0; i < splitStr.length; i++) {
-      // You do not need to check if i is larger than splitStr length, as your for does that for you
-      // Assign it back to the array
+  let splitStr = str.toLowerCase().split(' ');
+  for (let i = 0; i < splitStr.length; i++) {
       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
   }
-  // Directly return the joined string
   return splitStr.join(' '); 
 }
 
