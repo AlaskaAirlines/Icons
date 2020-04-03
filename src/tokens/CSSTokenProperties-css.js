f94846ffc3c6bb0bebb27da698f589d318e121ba
Dale Sande <dale.sande@alaskaair.com>
2020-04-03 11:23:08 -0700

    ci: address missing dependency

---
+++ src/tokens/CSSTokenProperties-css.js
@@ -0,0 +1,6 @@
+// This is a deprecated file that is only required to maintain backwards compatibility
+// until all components that have this dependency are updated.
+
+import {html} from 'lit-element';
+export default html`<style>:host{--size-icon-square-lg: 1.5rem;--size-icon-square-med: 1rem;--size-icon-square-sml: 0.6rem}
+</style>`;
