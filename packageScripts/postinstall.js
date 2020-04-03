3157b30b7fc74d995f73d77cf4224fd5626422d1
Dale Sande <dale.sande@alaskaair.com>
2020-04-03 11:23:08 -0700

    ci: address missing dependency

--- packageScripts/postinstall.js
+++ packageScripts/postinstall.js
@@ -3,11 +3,6 @@
 const chalk = require('chalk');
 const pjson = require('../package.json');
 
-// check for installed version of dependency
-const installedhyperlink = pjson.dependencies['@alaskaairux/ods-hyperlink'] || pjson.devDependencies['@alaskaairux/ods-hyperlink'];
-// const devInstalledhyperlink = pjson.devDependencies['@alaskaairux/ods-hyperlink'];
-const hyperlinkRequiredVersion = '^1.4.4'
-
 console.log(chalk.hex('#f26135')(`
 ╭ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ──────────────────────────────╮
 
@@ -17,19 +12,3 @@ console.log(chalk.hex('#f26135')(`
 ╰─────────────────────────────── ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─╯
 `)
 );
-
-if (installedhyperlink < hyperlinkRequiredVersion) {
-  console.log(chalk.hex('#f26135')(`
-╭ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ──────────────────────────────╮
-
-                   !!!!! ALERT !!!!!
-
-        It's been detected that you are running
-                  `) + chalk.hex('#ffd200').bold(`ods-hyperlink v${installedhyperlink}.`) + chalk.hex('#f26135')(`
-            `) + chalk.hex('#ffd200').bold(`ods-hyperlink v${hyperlinkRequiredVersion}`) + chalk.hex('#f26135')(` is required.
-
-            npm update @alaskaairux/ods-hyperlink
-
-╰─────────────────────────────── ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─╯
-`))
-}
