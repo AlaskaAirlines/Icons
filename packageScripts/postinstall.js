a951afe7a7bb63b578ac8717ad22c67978633fcb
Dale Sande <dale.sande@gmail.com>
2021-05-07 15:40:32 -0700

    feat(fs icons): add new flight search icons #83

--- packageScripts/postinstall.js
+++ packageScripts/postinstall.js
@@ -4,10 +4,28 @@ const chalk = require('chalk');
 const pjson = require('../package.json');
 
 console.log(chalk.hex('#f26135')(`
+
+ _______                   __           __ __
+|     __|.---.-.--.--.    |  |--.-----.|  |  |.-----.
+|__     ||  _  |  |  |    |     |  -__||  |  ||  _  |
+|_______||___._|___  |    |__|__|_____||__|__||_____|
+               |_____|
+ __              _______                    __
+|  |_.-----.    |   _   |.--.--.----.-----.|  |
+|   _|  _  |    |       ||  |  |   _|  _  ||__|
+|____|_____|    |___|___||_____|__| |_____||__|
+
+
 ╭ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ──────────────────────────────╮
 
         Thanks for installing the latest version
-                of `) + chalk.hex('#ffd200').bold(`Auro Icons v${pjson.version}.`) + chalk.hex('#f26135')(`
+                of `) + chalk.hex('#ffd200').bold(`Auro Icons v${pjson.version}.
+
+       The following icon(s) have been deprecated`) + chalk.hex('#f26135')(`
+           - interface/gov-approval.svg
+
+       Deprecated icon(s) will be removed with the
+        next MAJOR release. You have been warned.
 
 ╰─────────────────────────────── ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─╯
 `)
